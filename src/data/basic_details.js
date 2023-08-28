6da1b2730a031221ccf9b7d71a5e25e0c73f3286
rameshkumar.rajarath <rameshkumar.rajarathanam@infimotiontec.com>
2023-08-28 08:54:17 +0200

    Bug fix

--- src/data/basic_details.js
+++ src/data/basic_details.js
@@ -26,7 +26,7 @@ export const slidesText = {
 };
 
 export const homeText = {
-    home1Title: ' Ramesh Flexible innovation',
+    home1Title: ' Simteslab Flexible innovative testing Solution',
     home2Title: 'Grow with your system',
   home1: `Simtestlab delivers modular and future-proof test solutions to all kinds of industries around the world. We design test solutions with short lead times, ready to be integrated into your existing test environment. We offer both off-the-shelf and customized solutions based on your own requirements. We have partnered with NI for their excellent hardware and software platforms. We are also a member of ASAM to be actively involved in the future standards of test and measurement. From our offices around the globe, we offer onsite support to enable a long-term relationship with our customers. Our customers used to spend days re-configuring or re-designing test systems after each project or program. We’ve saved them both TIME and COSTS by smartly re-using hardware and software between projects or across product life-cycles. Join them and start your partnership with Simtestlab today!`,
   home2: `Simtestlab delivers modular and future-proof test solutions to all kinds of industries around the world. We design test solutions with short lead times, ready to be integrated into your existing test environment. We offer both off-the-shelf and customized solutions based on your own requirements. We have partnered with NI for their excellent hardware and software platforms. We are also a member of ASAM to be actively involved in the future standards of test and measurement. From our offices around the globe, we offer onsite support to enable a long-term relationship with our customers. Our customers used to spend days re-configuring or re-designing test systems after each project or program. We’ve saved them both TIME and COSTS by smartly re-using hardware and software between projects or across product life-cycles. Join them and start your partnership with Simtestlab today!`,
@@ -34,7 +34,7 @@ export const homeText = {
 
 
 export const aboutText = {
-  title: "The New Way toadvance.",
+  title: "The New Way to advance.",
   para: `Simtestlab was founded in Gothenburg, Sweden in 2020 by a group of people with different expertise in software testing, hardware development and entrepreneurial drive. We all experienced the increased need for more flexible and modular test solutions combined with the desire to have information accessible 24/7 through the cloud.
   
   . Our aim is to become the most open and qualified test system provider on the market. We focus on delivering high quality, durable solutions over rapid growth and profits. We have a focus to ensure that our colleagues will have a secure, inspired and stable workplace by signing a collective bargaining agreement.`
